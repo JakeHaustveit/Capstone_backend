83aaa23970361e4cefc1112f098603af04956faa
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-12-01 17:42:31 -0500

    Added owner, employee roles, employees, and joblist to models page

--- business_app/business/models.py
+++ business_app/business/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Owner(models.Model):
+    business_name = models.CharField(max_length=400)
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    employee_login_code = models.CharField(max_length=400)
+
+class EmployeeRoles(models.Model):
+    labour_code = models.CharField(max_length=50)
+
+class Employees(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    labour_code=ForeignKey(EmployeeRoles, on_delete=models.CASCADE)
+
+class JobList(models.Model):
+    job_name = models.CharField(max_length=400)
+    job_site = models.CharField(max_length=400)
+    job_start_date= models.IntegerField(blank= True, null=True)
+    job_end_date= models.IntegerField(blank= True, null=True)
+    business_name= models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+
